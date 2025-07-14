712104770073320fb03b4e9aad982c644c188968
Makwana Jaydeep <100516865+makwana-jaydeep@users.noreply.github.com>
2025-07-14 19:32:17 +0530

    Update test.py
    remove unnnecessy comment

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from googletrans import Translator
 
 def translate_text(text):
     translator = Translator()
-    translated = translator.translate(text, src="en", dest="gu")  # ❌ NO await
+    translated = translator.translate(text, src="en", dest="gu")  #  NO await
     return translated.text 
 
 if __name__ == "__main__":
